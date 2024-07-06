199bcee2c274dbf9359c040ee92f3c0fa090b4d7
Ilmiddin O'rinov <ilmiddinorino1@gmail.com>
2024-07-05 20:39:00 -0700

    chat app

--- app/src/main/java/com/ilmiddin1701/chatapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/ilmiddin1701/chatapp/fragments/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.ilmiddin1701.chatapp.fragments
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.view.ContextMenu
@@ -9,7 +8,6 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -25,7 +23,6 @@ import com.google.firebase.database.ValueEventListener
 import com.ilmiddin1701.chatapp.R
 import com.ilmiddin1701.chatapp.adapters.UsersAdapter
 import com.ilmiddin1701.chatapp.databinding.FragmentHomeBinding
-import com.ilmiddin1701.chatapp.models.MyMessage
 import com.ilmiddin1701.chatapp.models.Users
 import com.ilmiddin1701.chatapp.utils.MySharedPreferences
 import com.squareup.picasso.Picasso
